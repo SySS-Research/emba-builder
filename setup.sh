a53e767e5ee9cb66fda5d40b0f0b7a1a33c9320c
Sebastian Hamann <Sebastian.Hamann@SySS.de>
2025-02-19 17:20:14 +0100

    Kali-VM: Add persisten SSH host keys

--- setup.sh
+++ setup.sh
@@ -11,3 +11,7 @@ git submodule update --init --recursive
 for patch in "$(pwd)"/kali-vm-patches/*.patch; do
     git -C kali-vm apply "${patch}"
 done
+
+# Generate persistent SSH host keys.
+mkdir -p kali-vm/overlays/ssh-keys/etc/ssh/
+ssh-keygen -A -f kali-vm/overlays/ssh-keys/
