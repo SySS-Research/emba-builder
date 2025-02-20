53302e1276052f40bd9cd410c795ccde1bae0f85
Sebastian Hamann <Sebastian.Hamann@SySS.de>
2025-02-20 11:14:33 +0100

    Improve handling of already applied patches
    Unfortunately, git apply fails if a patch as already been applied.
    Instead of aborting setup.sh on the first failed patch, we try them all
    and inform the user that something may or may not be wrong.

--- setup.sh
+++ setup.sh
@@ -4,13 +4,29 @@ set -eu
 
 cd -- "$(dirname -- "$0")"
 
+WARN=
+NORMAL=
+if [[ -t 1 ]] && command -v tput &>/dev/null; then
+    WARN="$(tput setaf 3)"
+    NORMAL="$(tput op)"
+fi
+
 # Fetch submodules (i.e. kali-vm repository).
 git submodule update --init --recursive
 
 # Apply patches.
+failed_patches=()
 for patch in "$(pwd)"/kali-vm-patches/*.patch; do
-    git -C kali-vm apply "${patch}"
+    if ! git -C kali-vm apply "${patch}"; then
+        failed_patches+=( "${patch##*/}" )
+    fi
 done
+if [[ "${#failed_patches[@]}" -gt 0 ]]; then
+    printf '%sWarning: The following %d patches failed to apply. Check if this is expected!%s\n' "${WARN}" "${#failed_patches[@]}" "${NORMAL}"
+    for patch in "${failed_patches[@]}"; do
+        printf '%s* %s%s\n' "${WARN}" "${patch}" "${NORMAL}"
+    done
+fi
 
 # Generate persistent SSH host keys.
 mkdir -p kali-vm/overlays/ssh-keys/etc/ssh/
